36d1dec6cf98a45038c5ca7a468f71b6f76a9c0d
miekale <miekale.smith@gmail.com>
2025-03-30 19:23:16 -0400

    stopping after t

--- q1.py
+++ q1.py
@@ -5,9 +5,9 @@ from heat_solver import iterate, plot_temperature
 
 
 if __name__ == "__main__":
-    t_range = (0, 1000)
-    t_grid_size = 3
-    x_range = (0, 0.05)
+    t_range = (0, 10000)
+    t_grid_size = 1
+    x_range = (0, 0.048)
     x_grid_size = 0.001
 
     t_grid_count = int((t_range[1] - t_range[0]) / t_grid_size) + 1
@@ -16,19 +16,21 @@ if __name__ == "__main__":
     t_grid = np.linspace(t_range[0], t_range[1], t_grid_count)
     x_grid = np.linspace(x_range[0], x_range[1], x_grid_count)
 
-    k = 0.479 # thermal conductivity [W/mk]
+    k = 0.479 # thermal conductivity [W/mK]
     p = 1176  # density [kg/m^3]
-    c_p = 2893 # specific heat  [j/Kg*k]
+    c_p = 2893 # specific heat  [j/Kg*K]
     alpha = k / p / c_p
 
     F = t_grid_size / (x_grid_size**2) * alpha
     print(F)
-    print(1-2*F, " should be bigger than 0")
+    if abs(F) > .5:
+        raise Exception(f"F is {F}, should be bigger than 0.5")
 
-    space = np.full((t_grid_count, x_grid_count), 275)  # 275 K intial temp
+    space = np.full((t_grid_count, x_grid_count), 275, dtype=np.float128)  # 275 K intial temp
     space[0,:] = 275
     space[:,0] = 373.15
     space[:,-1] = 373.15
+
     print(space.shape)
     print("=============")
     np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
