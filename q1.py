eb6fc73e742132be5b4c8efe401c3928453bb9a8
miekale <miekale.smith@gmail.com>
2025-03-29 20:48:18 -0400

    seperating files, adding time plots

--- q1.py
+++ q1.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from heat_solver import iterate, plot_times
+from heat_solver import iterate, plot_temperature
 
 
 if __name__ == "__main__":
     t_range = (0, 1000)
-    t_grid_size = 0.001
-    x_range = (0, 1)
-    x_grid_size = 0.1
+    t_grid_size = 3
+    x_range = (0, 0.05)
+    x_grid_size = 0.001
 
     t_grid_count = int((t_range[1] - t_range[0]) / t_grid_size) + 1
     x_grid_count = int((x_range[1] - x_range[0]) / x_grid_size) + 1
@@ -16,23 +16,21 @@ if __name__ == "__main__":
     t_grid = np.linspace(t_range[0], t_range[1], t_grid_count)
     x_grid = np.linspace(x_range[0], x_range[1], x_grid_count)
 
-    alpha = 2
+    k = 0.479 # thermal conductivity [W/mk]
+    p = 1176  # density [kg/m^3]
+    c_p = 2893 # specific heat  [j/Kg*k]
+    alpha = k / p / c_p
 
     F = t_grid_size / (x_grid_size**2) * alpha
     print(F)
     print(1-2*F, " should be bigger than 0")
 
-    # Initial Conditions
-    space = np.zeros((t_grid_count, x_grid_count), dtype=np.float64)  
-    space[0,:] = np.cos(np.pi * x_grid) # u(t=0) = cos(pix)
-    space[:,0] = 0
-    space[:,-1] = 2
-    print(space.shape, x_grid.shape)
-
+    space = np.full((t_grid_count, x_grid_count), 275)  # 275 K intial temp
+    space[0,:] = 275
+    space[:,0] = 373.15
+    space[:,-1] = 373.15
     print(space.shape)
     print("=============")
     np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
     iterate(space, F)
-    plot_times([0.001,0.01,0.1,10], t_grid_size, space, x_grid)
-
-
+    plot_temperature(space)
