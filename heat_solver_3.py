20061832a7f54c30287602764f410177a505de4b
ooodette <152298305+ooodette@users.noreply.github.com>
2025-04-05 13:54:17 -0400

    updated heat solver (final)

--- heat_solver_3.py
+++ heat_solver_3.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 from egg_equation import get_sphere_radius_of_egg 
 
 # Parameters
-t = 800  # s total simulation time
-r_points = 100
+t_chick = 900  # s total simulation time
+t_os = 7000
+t_quail = 300
+r_points = 10
 dt = 0.1  # s
-k = 0.3370  # thermal conductivity W/mK
-p = 1036  # density [kg/m^3]
-c_p = 2093  # specific heat [j/Kg*K]
+k = 0.499  # thermal conductivity W/mK
+p = 1175.04  # density [kg/m^3]
+c_p = 3026.05  # specific heat [j/Kg*K]
 alpha = k / p / c_p
 
 t_water = 100  # °C
@@ -27,7 +29,7 @@ rad_ostrich = get_sphere_radius_of_egg(ostrich_egg_dimens)
 
 
 def egg_temperature(r, t, r_points, dt, alpha, t_water, t_init):
-    r = r * 100
+    r = r / 100
     dr = r / r_points
     t_points = int(t / dt)
     
@@ -66,27 +68,70 @@ def egg_temperature(r, t, r_points, dt, alpha, t_water, t_init):
     
     return temp, end_index
 
-
+###CHICKEN
 # Run simulation
-temp, end_index = egg_temperature(rad_chicken, t, r_points, dt, alpha, t_water, t_init)
-
-# Display maximum temperature
-print(np.max(temp[:, -1]))
+temp_chick, end_index_chick = egg_temperature(rad_chicken, t_chick, r_points, dt, alpha, t_water, t_init)
 
 # Extract temperature at the center (r = 0)
-temp_center = temp[0, :]
+temp_center_chick = temp_chick[0, :]
 
-time = np.linspace(0, t, len(temp_center))
+time = np.linspace(0, t_chick, len(temp_center_chick))
 
-final_time = (end_index) * dt if end_index > 0 else t
+final_time_chick = (end_index_chick) * dt if end_index_chick > 0 else t_chick
 
-print(f'The egg is fully cooked at t = {final_time:.1f} seconds')
+print(f'The chicken egg is fully cooked at t = {final_time_chick:.1f} seconds or or t = {final_time_chick/60:.1f} minutes')
 
 # Plot
 plt.figure(figsize=(10, 6))
-plt.plot(time, temp_center, linewidth=2)
+plt.plot(time, temp_center_chick, linewidth=2)
 plt.xlabel('Time [s]')
 plt.ylabel('Center Temperature [°C]')
-plt.title('Temperature at the Center of the Egg vs Time')
+plt.title('Temperature at the Center of the Chicken Egg vs Time')
+plt.grid(True)
+plt.show()
+
+
+###QUAIL
+# Run simulation
+temp_quail, end_index_quail = egg_temperature(rad_quail, t_quail, r_points, dt, alpha, t_water, t_init)
+
+# Extract temperature at the center (r = 0)
+temp_center_quail = temp_quail[0, :]
+
+time = np.linspace(0, t_quail, len(temp_center_quail))
+
+final_time_quail = (end_index_quail) * dt if end_index_chick > 0 else t_quail
+
+print(f'The quail egg is fully cooked at t = {final_time_quail:.1f} seconds or or t = {final_time_quail/60:.1f} minutes')
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.plot(time, temp_center_quail, linewidth=2)
+plt.xlabel('Time [s]')
+plt.ylabel('Center Temperature [°C]')
+plt.title('Temperature at the Center of the Quail Egg vs Time')
+plt.grid(True)
+plt.show()
+
+
+###OSTRICH
+# Run simulation
+temp_os, end_index_os = egg_temperature(rad_ostrich, t_os, r_points, dt, alpha, t_water, t_init)
+
+# Extract temperature at the center (r = 0)
+temp_center_os = temp_os[0, :]
+
+time = np.linspace(0, t_os, len(temp_center_os))
+
+final_time_os = (end_index_os) * dt if end_index_os > 0 else t_os
+
+print(f'The ostrich egg is fully cooked at t = {final_time_os:.1f} seconds or or t = {final_time_os/60:.1f} minutes')
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.plot(time, temp_center_os, linewidth=2)
+plt.xlabel('Time [s]')
+plt.ylabel('Center Temperature [°C]')
+plt.title('Temperature at the Center of the Ostrich Egg vs Time')
 plt.grid(True)
 plt.show()
