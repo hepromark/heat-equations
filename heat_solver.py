9bf4dad425d5b7b002421d0b05b5befe1eaf23fb
miekale <miekale.smith@gmail.com>
2025-04-06 22:05:07 -0400

    new

--- heat_solver.py
+++ heat_solver.py
@@ -88,20 +88,18 @@ def plot_temperature(space: np.ndarray):
 
 def analytical_solver(times: List[float], order: int = 100, num_points: int = 100):
     def C_n(n : int):
-        if n % 2 == 0:
-            return -4 / n / np.pi
+        if n == 1:
+            return -4 / np.pi
         else:
-            return 4 / n / np.pi + 4 * n / np.pi / (n**2 - 1)
+            return (1 - np.cos((n+1)*np.pi))/ (n+1) / np.pi - (1-np.cos((1-n)*np.pi)) / (n-1)/np.pi + 4*np.cos(np.pi*n) / n / np.pi
 
     def f_x(x : np.array, t : np.array, order : int) -> float:
-        if order < 2:
+        if order < 1:
             raise Exception("Order must be at least 2")
         # Shape: (len(t), len(x))
-        values = np.zeros((x.shape[0], t.shape[0]))
-
         values =  2 * np.tile(x, (t.shape[0], 1))
 
-        for n in range(2, order + 1):
+        for n in range(1, order + 1):
             coeff = C_n(n)
             sin_term = np.sin(np.pi * n * x)  # shape: (len(x),)
             decay = np.exp(-2 * (np.pi * n)**2 * t[:, None])  # shape: (len(t), 1)
